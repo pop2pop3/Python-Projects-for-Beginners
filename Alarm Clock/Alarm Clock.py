5b8c8b8072405ff64b2fdde4b052403d577f3698
Muhammad Lufti Apriliyanto <86290293+pop2pop3@users.noreply.github.com>
2023-04-21 04:22:03 +0800

    Update Alarm Clock.py
    Modify the `set_alarm` function to run in another thread using the `threading` module in Python.
    
    In this modified code, the check_alarm function is defined inside the set_alarm function, and then a new thread is started with check_alarm as the target. The main set_alarm function returns immediately after starting the new thread, so it doesn't block the execution of the calling script.
    
    To use this modified set_alarm function in another script, you can simply import it and call it as follows:
    
    ```
    from alarm_clock import set_alarm
    
    alarm_time = input("Enter time in 'HH:MM:SS AM/PM' format: ")
    set_alarm(alarm_time)
    
    # your other code here...
    ```
    
    This way, the alarm clock will run in the background while the rest of your script continues to execute.

--- Alarm Clock/Alarm Clock.py
+++ Alarm Clock/Alarm Clock.py
@@ -1,6 +1,37 @@
-# Importing required libraries
-from datetime import datetime   #To set date and time
-from playsound import playsound     #To play sound
+from threading import Thread
+from datetime import datetime
+from playsound import playsound
+
+def set_alarm(alarm_time):
+    validate = validate_time(alarm_time.lower())
+    if validate != "ok":
+        print(validate)
+        return
+    print(f"Setting alarm for {alarm_time}...")
+
+    alarm_hour = alarm_time[0:2]
+    alarm_min = alarm_time[3:5]
+    alarm_sec = alarm_time[6:8]
+    alarm_period = alarm_time[9:].upper()
+
+    def check_alarm():
+        while True:
+            now = datetime.now()
+
+            current_hour = now.strftime("%I")
+            current_min = now.strftime("%M")
+            current_sec = now.strftime("%S")
+            current_period = now.strftime("%p")
+
+            if alarm_period == current_period:
+                if alarm_hour == current_hour:
+                    if alarm_min == current_min:
+                        if alarm_sec == current_sec:
+                            playsound('D:/Library/Documents/Projects/Coding/Beginner Python Projects/Alarm Clock/alarm.wav')
+                            break
+
+    t = Thread(target=check_alarm)
+    t.start()
 
 def validate_time(alarm_time):
     if len(alarm_time) != 11:
@@ -14,34 +45,3 @@ def validate_time(alarm_time):
             return "Invalid SECOND format! Please try again..."
         else:
             return "ok"
-
-while True:
-    alarm_time = input("Enter time in 'HH:MM:SS AM/PM' format: ")
-    
-    validate = validate_time(alarm_time.lower())
-    if validate != "ok":
-        print(validate)
-    else:
-        print(f"Setting alarm for {alarm_time}...")
-        break
-
-alarm_hour = alarm_time[0:2]
-alarm_min = alarm_time[3:5]
-alarm_sec = alarm_time[6:8]
-alarm_period = alarm_time[9:].upper()
-
-while True:
-    now = datetime.now()
-
-    current_hour = now.strftime("%I")
-    current_min = now.strftime("%M")
-    current_sec = now.strftime("%S")
-    current_period = now.strftime("%p")
-
-    if alarm_period == current_period:
-        if alarm_hour == current_hour:
-            if alarm_min == current_min:
-                if alarm_sec == current_sec:
-                    print("Wake Up!")
-                    playsound('D:/Library/Documents/Projects/Coding/Beginner Python Projects/Alarm Clock/alarm.wav')
-                    break
